8cae2acd7fcd42c7ac2450239ba70766a53c8f04
M Refi Dwi Ananta <refi@blajr.xyz>
2023-05-07 17:28:20 +0800

    initial Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [require('@tailwindcss/typography'), require("daisyui")],
+	daisyui: {
+		styled: true,
+		themes: true,
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: "",
+		darkTheme: "cmyk",
+	  },
+	  fontFamily: {
+		  'sans': ['Quicksand'],
+		  'display': ['Pacifico'],
+	  },
+
+}
